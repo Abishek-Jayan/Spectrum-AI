219b9fe8469cbd336eda61f9005eebb590adce2f
Abishek Jayan <abishekjayan98@gmail.com>
2023-07-10 15:07:18 +0530

    Fixed input issue

--- src/pages/homepage/components/Dashboard/index.tsx
+++ src/pages/homepage/components/Dashboard/index.tsx
@@ -3,17 +3,28 @@ import { Slider } from "@material-tailwind/react";
 import PopupDialog from "../../../../components/Popup";
 
 class Dashboard extends React.Component {
+    state = {
+        input_text: "The dragon stole the prince and..."
+    }
+    handleChange (value:any) {
+        this.setState(prevState=>{
+            return {
+                ...prevState,
+                input_text: value
+            }
+        })
+    }
     render() {
         return <>
-            <div className="flex flex-col items-center justify-center mt-56 space-y-20">
-                <input className="w-96 h-10 border-yellow-600 rounded-md border-2 bg-gray-900" type="text" value="The dragon stole the prince and..."/>
+            <section className="flex flex-col mt-56 items-center justify-center space-y-20">
+                <input className="w-96 h-10 border-yellow-600 rounded-md border-2 bg-gray-900" type="text" value={this.state.input_text} onChange={e=>{this.handleChange(e.target.value)}}/>
                 <div className="flex space-x-10">
                     <p>Precise</p>
                 <Slider className="w-96 h-0" size="lg"defaultValue={50} />
                     <p>Diverse</p>
                 </div>
                 <PopupDialog buttonName="Go"/>
-            </div>        
+            </section>        
         </>
     }
 }
